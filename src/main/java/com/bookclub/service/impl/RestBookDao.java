672bb0cc4e4945c3e95781f2a7d94e0efc22ee93
Jessi <jperreault@my365.bellevue.edu>
2024-05-05 16:01:07 -0700

    Assignment 8, Updated Landing Page, Selected Book, and Web Link using API

---
+++ src/main/java/com/bookclub/service/impl/RestBookDao.java
@@ -0,0 +1,69 @@
+import com.bookclub.model.Book;
+import com.bookclub.service.dao.BookDao;
+import com.jayway.jsonpath.JsonPath;
+import org.springframework.web.util.UriComponentsBuilder;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+import com.jayway.jsonpath.Configuration;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RestBookDao implements BookDao
+{
+
+    public List<Book> list()
+    {
+        String isbnString = "ISBN:9780593099322,9780261102361,9780261102378,9780590302715,9780316769532";
+        Object doc = getBooksDoc(isbnString);
+        List<Book> books = new ArrayList<Book>();
+        List<String> titles = JsonPath.read(doc, "$..title");
+        List<String> isbns = JsonPath.read(doc, "$..bib_key");
+        List<String> infoUrls = JsonPath.read(doc, "$..info_url");
+        for (int index = 0; index < titles.size(); index++)
+        {
+            books.add(new Book(isbns.get(index), titles.get(index), infoUrls.get(index)));
+        }
+        return books;
+    }
+
+    public Book find(String key)
+    {
+        Object doc = getBooksDoc(key);
+        List<String> isbns = JsonPath.read(doc, "$..bib_key");
+        List<String> titles = JsonPath.read(doc, "$..title");
+        List<String> subtitle = JsonPath.read(doc, "$..details.subtitle");
+        List<String> infoUrls = JsonPath.read(doc, "$..info_url");
+        List<String> pages = JsonPath.read(doc, "$..details.number_of_pages");
+        String isbn = isbns.size() > 0 ? isbns.get(0) : "N/A";
+        String title = titles.size() > 0 ? titles.get(0) : "N/A";
+        String desc = subtitle.size() > 0 ? subtitle.get(0) : "N/A";
+        int numOfPages = pages.size() > 0 ? Integer.parseInt(pages.get(0)) : 0;
+        String infoUrl = infoUrls.size() > 0 ? infoUrls.get(0) : "N/A";
+        Book book = new Book(isbn, title, desc, infoUrl, numOfPages);
+        return book;
+    }
+
+    public List<Book> getBooksDoc(String isbnString)
+    {
+        String openLibraryUrl = "https://openlibrary.org/api/books";
+        RestTemplate rest = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.set(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE);
+        HttpEntity<?> entity = new HttpEntity<>(headers);
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(openLibraryUrl)
+                .queryParam("bibkeys", isbnString)
+                .queryParam("format", "json")
+                .queryParam("jscmd", "details");
+        ResponseEntity<String> response = rest.exchange(
+                builder.toUriString(),
+                HttpMethod.GET,
+                entity,
+                String.class);
+        String jsonBookList = response.getBody();
+        return Configuration.defaultConfiguration().jsonProvider().parse(jsonBookList);
+    }
+}
